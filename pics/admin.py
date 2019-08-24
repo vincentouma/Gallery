05c67308d1eb0c6a4c48f6d84f3c7714dcc1ce25
vincentouma <vinceoumah@gmail.com>
2019-08-24 15:35:28 +0300

    add image admin

--- pics/admin.py
+++ pics/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-# from .models import Image,Place,Category
+from .models import Image,Place,Category
 
 # Register your models here.
 
-# admin.site.register(Image)
-# admin.site.register(Place)
-# admin.site.register(Category)
+# class ImageAdmin(admin.ModelAdmin):
+#     filter_horizontal =('',)
+
+admin.site.register(Image,)
+admin.site.register(Place)
+admin.site.register(Category)
