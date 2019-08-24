1f0c9ed1680dcfe8cf70b9f5cfabf6c4102b6e4d
vincentouma <vinceoumah@gmail.com>
2019-08-24 10:58:53 +0300

    add model to admin

--- pics/admin.py
+++ pics/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Image,Place,Category
 
 # Register your models here.
+
+admin.site.register(Image)
+admin.site.register(Place)
+admin.site.register(Category)
