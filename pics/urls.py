2bd4ea979b1acea40ddcf41a0222887fe5e7e6ac
vincentouma <vinceoumah@gmail.com>
2019-08-25 22:54:59 +0300

    make migration

--- pics/urls.py
+++ pics/urls.py
@@ -4,7 +4,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.gallery,name = 'picsGallery'),
+    url(r'^$', views.pics, name = 'welcome'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^picture/(\d+)', views.filtered, name='filtered'),
+
+
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- pics/views.py
+++ pics/views.py
@@ -1,12 +1,39 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
+from .models import Picture
+
 
 # Create your views here.
+def welcome(request):
+    return render(request, 'welcome.html')
 
-
-def gallery(request):
+def picture_of_day(request):
     date = dt.date.today()
-    pics = Image.gallery()
-    return render(request, 'all-pics/gallery.html', {"date": date,"news":news})
+    pictures = Picture.todays_picture()
+    return render(request, 'picture.html', {"date": date, "pictures": pictures})
+
+def filtered(request,past_date):
+    picture = Picture.objects.get(id = past_date)
+    return render(request, 'all-pictures/picture.html', {"picture":picture})
+
+def search_results(request):
+    
+
+    if 'picture' in request.GET and request.GET["picture"]:
+        search_term = request.GET.get("picture")
+        searched_pictures = Picture.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-pictures/search.html',{"message":message,"pictures": searched_pictures})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-pictures/search.html',{"message":message})
+
+def pics(request):
+   
+    pictures = Picture.objects.all()
+
+    return render(request,"welcome.html", {"pictures":pictures})
 
