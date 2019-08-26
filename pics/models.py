7f5d6a78c62d9edd76c5257bdaf202e4946cee0a
vincentouma <vinceoumah@gmail.com>
2019-08-26 15:31:55 +0300

    midify models

--- pics/models.py
+++ pics/models.py
@@ -25,7 +25,7 @@ class Category(models.Model):
 
 
 class Picture(models.Model):
-    picture = models.ImageField(upload_to='picture/',null=True)
+    picture = models.ImageField(upload_to='picture/',default='DEFAULT VALUE')
     name = models.CharField(max_length=60)
     details = models.TextField()
     category = models.ForeignKey(Category)
@@ -33,21 +33,20 @@ class Picture(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
 
 
-    # def __str__(self):
-    #     return self.picture
+    
 
     def save_picture(self):
         self.save()
 
     @classmethod
     def todays_picture(cls):
-        today = dt.date.today()
+        today = dt.today()
         picture = cls.objects.filter(pub_date__date = today)
         return picture
 
     @classmethod
     def search_by_category(cls,search_term):
-        picture = cls.objects.filter(category__icontains=search_term)
+        picture = cls.objects.filter(category__category__icontains=search_term)
         return picture
 
     @classmethod
