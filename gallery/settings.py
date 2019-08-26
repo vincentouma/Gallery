56dd6694b8229f0deff34acaf3442599c5982bf0
vincentouma <vinceoumah@gmail.com>
2019-08-26 16:32:04 +0300

    deployment

--- gallery/settings.py
+++ gallery/settings.py
@@ -93,7 +93,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+)
 
 ROOT_URLCONF = 'gallery.urls'
 
