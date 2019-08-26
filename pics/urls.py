1a1ac0af8772b8c4a03b0a6709a040bf408a335c
vincentouma <vinceoumah@gmail.com>
2019-08-26 15:34:08 +0300

    change view url

--- pics/urls.py
+++ pics/urls.py
@@ -4,9 +4,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^$', views.pics, name = 'welcome'),
+    url(r'^$', views.picture_of_day, name = 'welcome'),
     url(r'^search/', views.search_results, name='search_results'),
-    url(r'^picture/(\d+)', views.filtered, name='filtered'),
+    url(r'^pictures/(\d+)', views.filtered, name='filtered'),
 
 
 
